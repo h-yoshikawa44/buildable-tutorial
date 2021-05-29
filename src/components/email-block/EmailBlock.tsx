a35f9f018637acbc3c2dd0f489526f274c084e04
h-yoshikawa44 <43331308+h-yoshikawa44@users.noreply.github.com>
2021-05-29 12:25:23 +0900

    Buildableチュートリアル - Part 1B: Adding a skeleton loader

--- src/components/email-block/EmailBlock.tsx
+++ src/components/email-block/EmailBlock.tsx
@@ -1,52 +1,76 @@
-import React, { VFC } from 'react';
+import React, { FC, VFC, useState, useEffect } from 'react';
 import { Box, Text, Heading } from 'grommet';
 import { TextField } from '@material-ui/core';
 import theme from '../../theme';
 import Button from './Button';
+import LoadingBlock from './LoadingBlock';
 
 const { colors } = theme;
 
+const WrapperBox: FC = ({ children }) => (
+  <Box
+    elevation={'large'}
+    width={'500px'}
+    height={{ min: 'max-content' }}
+    round="8px"
+    background={colors.white}
+    pad={'large'}
+    gap={'medium'}
+  >
+    {children}
+  </Box>
+);
+
 const EmailBlock: VFC = () => {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
+
   return (
-    <Box
-      elevation={'large'}
-      width={'500px'}
-      height={{ min: 'max-content' }}
-      round="8px"
-      background={colors.white}
-      pad={'large'}
-      gap={'medium'}
-    >
-      <Heading level={1} color={colors.black}>
-        Become smarter in just 5 minutes
-      </Heading>
-      <Text size={'medium'}>
-        Get the daily email that makes reading the news actually enjoyable. Stay
-        informed and entertained, for free.
-      </Text>
-      <TextField
-        id="outlined-basic"
-        type={'email'}
-        label={'Enter your email'}
-        placeholder={'Enter your email'}
-        variant="outlined"
-      />
-      <Button
-        type="submit"
-        onClick={(e) => {
-          e.preventDefault();
-        }}
-        // disabled={isProcessing || !isValid}
-        background={colors.primary}
-        color={colors.white}
-        style={{
-          paddingTop: '16px',
-          paddingBottom: '16px',
-        }}
-      >
-        Submit
-      </Button>
-    </Box>
+    <>
+      {isLoading && (
+        <WrapperBox>
+          <LoadingBlock />
+        </WrapperBox>
+      )}
+      {!isLoading && (
+        <WrapperBox>
+          <Heading level={1} color={colors.black}>
+            Become smarter in just 5 minutes
+          </Heading>
+          <Text size={'medium'}>
+            Get the daily email that makes reading the news actually enjoyable.
+            Stay informed and entertained, for free.
+          </Text>
+          <TextField
+            id="outlined-basic"
+            type={'email'}
+            label={'Enter your email'}
+            placeholder={'Enter your email'}
+            variant="outlined"
+          />
+          <Button
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+            }}
+            // disabled={isProcessing || !isValid}
+            background={colors.primary}
+            color={colors.white}
+            style={{
+              paddingTop: '16px',
+              paddingBottom: '16px',
+            }}
+          >
+            Submit
+          </Button>
+        </WrapperBox>
+      )}
+    </>
   );
 };
 
